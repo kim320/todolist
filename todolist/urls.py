2991563ae6ed9b6471e9ebd367c68705d3c113a0
kim320 <hj_kim0320@likelion.org>
2019-05-18 19:34:20 +0900

    update todolist

--- todolist/urls.py
+++ todolist/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', todolistapp.views.todolist, name='todolist'),
     path('todolist/new', todolistapp.views.new, name='new'),
     path('todolist/create', todolistapp.views.create, name='create'),
-    #path('todolist/edit/<int:todolist_id>', todolistapp.views.edit, name='edit'),
     path('todolist/destroy/<int:todolist_id>', todolistapp.views.destroy, name='destroy'),
+    path('todolist/edit/<int:todolist_id>', todolistapp.views.edit, name='edit'),
+    path('todolist/update/<int:todolist_id>', todolistapp.views.update, name='update'),
 ]
--- todolistapp/views.py
+++ todolistapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404 , redirect
+from django.utils import timezone
 from .models import Todolist
 
 def todolist(request):
+    today = timezone.datetime.now()
     todolists = Todolist.objects
-    return render(request, 'todolist.html', {'todolists':todolists})
+    return render(request, 'todolist.html', {'todolists':todolists, 'today':today})
 
 def new(request):
     return render(request, 'new.html')
@@ -15,11 +17,17 @@ def create(request):
 
     todolist.save()
     return redirect('/')
-"""
+
 def edit(request, todolist_id):
     todolist = Todolist.objects.get(pk=todolist_id)
+    return render(request, 'edit.html', {'todolist':todolist})
+
+def update(request, todolist_id):
+    todolist = Todolist.objects.get(pk=todolist_id)
+    todolist.title = request.GET['title']
+    todolist.content = request.GET['content']
+    todolist.save()
     return redirect('/')
-"""
 
 def destroy(request, todolist_id):
     todolist = Todolist.objects.get(pk=todolist_id)
