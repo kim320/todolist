034d8fadb4617eb7a4a9f8de0189765a4e298be3
kim320 <hj_kim0320@likelion.org>
2019-05-19 10:13:30 +0900

    level todolist

--- todolist/urls.py
+++ todolist/urls.py
@@ -5,6 +5,8 @@ import todolistapp.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', todolistapp.views.todolist, name='todolist'),
+    #path('todolist/todo', todolistapp.views.todo, name='todo'),
+    #path('todolist/done', todolistapp.views.done, name='done'),
     path('todolist/new', todolistapp.views.new, name='new'),
     path('todolist/create', todolistapp.views.create, name='create'),
     path('todolist/destroy/<int:todolist_id>', todolistapp.views.destroy, name='destroy'),
--- todolistapp/models.py
+++ todolistapp/models.py
@@ -8,7 +8,7 @@ class Todolist(models.Model):
     done = models.BooleanField(default=False)
 
     due_date = models.DateField(blank=True, null=True)
-    level = models.CharField(max_length=2, blank=True, null=True)
+    level = models.CharField(max_length=3, default=None, blank=True, null=True)
     
     def __str__(self):
         return self.title
