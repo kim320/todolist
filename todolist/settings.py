761fb6f7ed6aedf08990cf716b5f51c903f98ae2
kim320 <hj_kim0320@likelion.org>
2019-05-20 16:05:22 +0900

    static edit

--- todolist/settings.py
+++ todolist/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
